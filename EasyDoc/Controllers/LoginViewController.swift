b5d478d753b7c105ffd3e97879657196362200a3
Rodrigo Hilkner <rodrigo.hilkner@gmail.com>
2018-01-23 04:47:27 -0200

    Initial Commit

---
+++ EasyDoc/Controllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyDoc
+//
+//  Created by Rodrigo Hilkner on 23/01/18.
+//  Copyright © 2018 Rodrigo Hilkner. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
+
